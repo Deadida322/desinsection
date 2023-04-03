7837a7895d5bcdda3e82cb294294f8e3b9dfdc47
Давид Алиев <61591520+Deadida322@users.noreply.github.com>
2023-04-03 20:39:12 +0400

    Add files via upload

---
+++ _nuxt/static/1680539849/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdesinsection\u002F_nuxt\u002Fstatic\u002F1680539849",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdesinsection\u002F",assetsPath:"\u002Fdesinsection\u002F_nuxt\u002F",cdnURL:a}}}}(null));
